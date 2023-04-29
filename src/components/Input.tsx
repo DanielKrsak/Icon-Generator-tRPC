bd5e3367dd8d61dd0ad845b836663d494d02731f
Daniel Krsak <krsak.daniel@gmail.com>
2023-04-29 14:58:50 +0200

    adding mutation endpoint, sending data from ui

---
+++ src/components/Input.tsx
@@ -0,0 +1,11 @@
+const Input = (props: React.ComponentPropsWithoutRef<"input">) => {
+  return (
+    <input
+      {...props}
+      type="text"
+      className="rounded border border-gray-800 px-4 py-2 text-black outline-none"
+    />
+  );
+};
+
+export default Input;
