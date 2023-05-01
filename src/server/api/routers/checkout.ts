c88e3fcb9977057d2b162f29614cce4507181a5e
Daniel Krsak <krsak.daniel@gmail.com>
2023-05-01 19:01:24 +0200

    code changes

--- src/server/api/routers/checkout.ts
+++ src/server/api/routers/checkout.ts
@@ -14,8 +14,8 @@ export const checkoutRouter = createTRPCRouter({
       },
       line_items: [{ price: env.PRICE_ID, quantity: 1 }],
       mode: "payment",
-      success_url: `${env.HOST_NAME}`,
-      cancel_url: `${env.HOST_NAME}`,
+      success_url: "/",
+      cancel_url: "/",
     });
   }),
 });
