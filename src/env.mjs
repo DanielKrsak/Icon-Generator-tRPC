bcffde6c296fd14b9eede168b3bc60695378f569
Daniel Krsak <krsak.daniel@gmail.com>
2023-05-01 18:59:42 +0200

    removed env

--- src/env.mjs
+++ src/env.mjs
@@ -13,13 +13,6 @@ export const env = createEnv({
       process.env.NODE_ENV === "production"
         ? z.string().min(1)
         : z.string().min(1).optional(),
-    NEXTAUTH_URL: z.preprocess(
-      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
-      // Since NextAuth.js automatically uses the VERCEL_URL if present.
-      (str) => process.env.VERCEL_URL ?? str,
-      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-      process.env.VERCEL ? z.string().min(1) : z.string().url()
-    ),
     // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
     GOOGLE_CLIENT_ID: z.string(),
     GOOGLE_CLIENT_SECRET: z.string(),
@@ -28,7 +21,6 @@ export const env = createEnv({
     SECRET_ACCESS_KEY: z.string(),
     ACCESS_KEY_ID: z.string(),
     STRIPE_SECRET_KEY: z.string(),
-    HOST_NAME: z.string(),
     PRICE_ID: z.string(),
     STRIPE_WEB_HOOK_SECRET: z.string(),
   },
@@ -51,7 +43,6 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     DALLE_API_KEY: process.env.DALLE_API_KEY,
@@ -60,7 +51,6 @@ export const env = createEnv({
     ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
     NEXT_PUBLIC_STRIPE_KEY: process.env.NEXT_PUBLIC_STRIPE_KEY,
     STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-    HOST_NAME: process.env.HOST_NAME,
     PRICE_ID: process.env.PRICE_ID,
     STRIPE_WEB_HOOK_SECRET: process.env.STRIPE_WEB_HOOK_SECRET,
   },
